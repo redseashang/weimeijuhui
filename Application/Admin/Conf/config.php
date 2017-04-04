ecf09a722d87ab8a12d317a88e9bae655be5e18c
redseashang <1823871319@qq.com>
2017-04-04 20:22:28 +0800

    Revert "01"
    This reverts commit 13b0f3e81283e653b1058069c9ddfaa619a2cee7.

--- Application/Admin/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	//'配置项'=>'配置值'
-    'TMPL_L_DELIM'          =>  '<{',            // 模板引擎普通标签开始标记
-    'TMPL_R_DELIM'          =>  '}>',            // 模板引擎普通标签结束标记
-    'AdminKey'              =>  'ASCV%^3343&%fh',
-    'AdminLoginTimeCheck'=>'3600',
-    //'LOAD_EXT_FILE'         => 'functions',// 动态载入 当前项目组中的公共文件
-);
